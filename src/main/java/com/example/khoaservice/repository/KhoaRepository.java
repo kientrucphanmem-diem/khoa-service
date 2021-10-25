aa234c00a968a64e9c39adcb5e83a1b8bd6b3f6a
diemmoon <duongdiem20@gmail.com>
2021-10-25 18:54:54 +0700

    euruka

--- src/main/java/com/example/khoaservice/repository/KhoaRepository.java
+++ src/main/java/com/example/khoaservice/repository/KhoaRepository.java
@@ -3,5 +3,6 @@ package com.example.khoaservice.repository;
 import com.example.khoaservice.entity.Khoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface KhoaRepository extends JpaRepository<Khoa, Integer> {
 }
